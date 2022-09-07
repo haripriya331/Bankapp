be10d687efbe8739023d2a1ffaa8e0f4d79fc03f
amdonb67 <amdonb67@Amdjfs136.iiht.tech>
2022-09-07 20:30:47 +0530

    initial

---
+++ src/banknewproject/Databconnect.java
@@ -0,0 +1,26 @@
+package banknewproject;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Databconnect {
+	static Connection con;
+	 public static Connection getConnection()
+	    {
+	        try {               
+	            String mysqlJDBCDriver = "oracle.jdbc.driver.OracleDriver"; 
+	            String url = "jdbc:oracle:thin:@localhost:1521/orcl.iiht.tech";
+	            String user = "scott";       
+	            String pass = "tiger"; 
+	            Class.forName(mysqlJDBCDriver);
+	            con = DriverManager.getConnection(url, user, pass);
+	        }
+	        catch (Exception e) {
+	            System.out.println("Connection Failed!");
+	        }
+	        return con;
+	    }
+}
+
